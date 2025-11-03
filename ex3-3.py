1f39ada106463dd15eda3ceedc1cc7f733c3128a
berneeyeh <berneeyeh@gmail.com>
2025-11-03 20:41:58 +0800

    Create ex3-3.py

---
+++ ex3-3.py
@@ -0,0 +1,12 @@
+# 整數 (Integer)
+age = input("please input int number = ")
+print("age = ",age)
+print(type(age))
+
+# 浮點數 (Float)
+height = 175.6
+
+print(type(height))    # <class 'float'>
+# 布林值 (Boolean)
+is_student = True
+print(type(is_student)) # <class 'bool'>
