7341f86a29de9a468d23828884197802c966cfb7
berneeyeh <berneeyeh@gmail.com>
2025-11-03 20:15:47 +0800

    Create ex3-2.py

---
+++ ex3-2.py
@@ -0,0 +1,18 @@
+# 字串 (String)
+name = "20"
+
+print(name)
+print(type(name))      # <class 'str'>
+# 整數 (Integer)
+age = 25
+name = 20
+print(type(age))       # <class 'int'>
+print(name)
+print(type(name))      # <class 'int'>
+# 浮點數 (Float)
+height = 175.6
+
+print(type(height))    # <class 'float'>
+# 布林值 (Boolean)
+is_student = True
+print(type(is_student)) # <class 'bool'>
